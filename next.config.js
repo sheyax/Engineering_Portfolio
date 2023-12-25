5adb2c0f18c4b5a39f93c7af5e7159950889248f
sheyax <olusheyax@gmail.com>
2023-12-25 02:20:10 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,19 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  distDir: 'dist',
+  output: "export",
+  distDir: "dist",
   images: {
-    unoptimized: true
-  }
-}
- 
-module.exports = nextConfig
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
